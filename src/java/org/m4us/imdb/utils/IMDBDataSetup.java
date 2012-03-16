1c33b3effe217c47bf0da4fb3d016560d8aa5d7f
Arka Dasgupta <arkadasgupta@gmail.com>
2012-03-15 20:54:21 -0500

    adding imdb utils file

---
+++ src/java/org/m4us/imdb/utils/IMDBDataSetup.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.m4us.imdb.utils;
+
+/**
+ *
+ * @author arka
+ */
+public class IMDBDataSetup {
+    public static void main(String[] args) {
+        
+    }
+}
